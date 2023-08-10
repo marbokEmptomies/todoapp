528e82e199e7c1ea02eff5af1a740742c2fe61a3
Marko Soramäki <114909768+marbokEmptomies@users.noreply.github.com>
2023-08-10 15:01:18 +0300

    single item

---
+++ src/TodoItem.jsx
@@ -0,0 +1,20 @@
+export function TodoItem({completed, id, title, toggleTodo, deleteTodo}) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          defaultChecked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button
+        onClick={() => deleteTodo(id)}
+        className="btn btn-danger"
+      >
+        Delete
+      </button>
+    </li>
+  );
+}
